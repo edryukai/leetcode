f6b38e6063259a25a649c530711e9090683c7f0c
edryukai <gouthamkgh.1337@gmail.com>
2018-09-11 17:24:31 +0530

    Adding alternative solution to LC539

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Problem ID  | Problem | Solution | Time | Space | Solved on | TTS (min) | Topic
 ------------|---------|----------|------------------|-----------------|-----------|------------------|-------
 200  | [Find number of islands](https://leetcode.com/problems/number-of-islands/) | [Click](../master/src/LC200.cs) | O(rc) | O(rc) | 10-Sept | 30 | Union-Find
 34   | [First and last occurance in sorted array](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/) | [Click](../master/src/LC34.cs) | O(log n) | O(1) | 11-Sept | 25 | Binary Search
-539  | [Find smallest time diff window](https://leetcode.com/problems/minimum-time-difference/) | [Click](../master/src/LC539.cs) | O(1) | O(1) | 11-Sept | 35 | Sliding Window
+539  | [Find smallest time diff window](https://leetcode.com/problems/minimum-time-difference/) | [Click](../master/src/LC539.cs) | O(1) | O(1) | 11-Sept | 35 | Sliding Window, Sort
 
 ## Problems tagged Hard
 Problem ID  | Problem | Solution | Time | Space | Solved on | TTS (min) | Topic
--- src/LC539.cs
+++ src/LC539.cs
@@ -2,10 +2,45 @@
 // Space: O(1) // considering 1440 elements as constant
 // Time: O(1) // considering 1440 elements as constant
 
-// Logic:
-// - Since there are 24*60 distinct timepoints, it makes sense to have a bool array to mark those timepoints that are existent in input
-// - Corner case:  23:59 and 00:03 should give 4 minutes instead of 1436 - i.e consider circularly
-//                 To deal with this, if I keep track of the first and last window indices that were found, I can check for 'circular' kind of distance to compare with min computed so far 
+// Two solutions:
+// Solution 1:
+//                  - Get int values of timestamps in minutes
+//                  - Sort the array of timestamps (should be O(n) sort as values will lie between 0 and 1440)
+//                  - Find min distance between timestamps
+//                  - Corner case: consider circular distance. Ex: 23:55 and 00:05 should give 10 minutes. 
+//                  - Time: O(n), Space: O(1)
+// Solution 2:
+//                  - Since there are 24*60 distinct timepoints, it makes sense to have a bool array to mark those timepoints that are existent in input
+//                  - Start with a small valid window, keep sliding the window to find smallest window size
+//                  - Corner case: consider circular distance. Ex: 23:55 and 00:05 should give 10 minutes. 
+//                  - Time: O(1), Space: O(1) since 1440 could be considered constant
+
+/* Solution - 1 */
+
+public class Solution {
+    public int FindMinDifference(IList<string> timePoints) {
+        List<int> intTimestamps = new List<int>();
+        
+        foreach(string x in timePoints) {
+            string[] ts = x.Split(':');
+            int curr = Convert.ToInt32(ts[0])*60 + Convert.ToInt32(ts[1]);
+            intTimestamps.Add(curr);
+        }
+        
+        int[] tsarray = intTimestamps.ToArray();
+        Array.Sort(tsarray);
+        
+        int min = Int32.MaxValue;
+        for(int i = 1; i < tsarray.Length; i++) {
+            min = Math.Min(min, tsarray[i]-tsarray[i-1]);
+        }
+        
+        // Corner case: checking for min distance circulary
+        return Math.Min(min, tsarray[0] + (24*60 - tsarray[tsarray.Length-1]));
+    }
+}
+
+/* Solution - 2 */
 
 public class Solution {
     public int FindMinDifference(IList<string> timePoints) {
